9dc3fdf716b7bc5738c833c01cdc0e4ffedb79ba
蒼時弦也 <elct9620@frost.tw>
2021-01-30 13:13:19 +0800

    Setup tensorflow for object detection

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -8,15 +8,18 @@
     <a-entity gltf-model="/models/ggj_prp_wep_001.gltf" position="0 -5 -30"></a-entity>
 
     <nearby-player />
+    <object-detection />
   </a-scene>
 </template>
 
 <script>
 import NearbyPlayer from 'components/nearby_player'
+import ObjectDetection from 'components/object_detection'
 
 export default {
   components: {
     'nearby-player': NearbyPlayer,
+    'object-detection': ObjectDetection
   },
   data: function () {
     return {
---
+++ app/javascript/components/object_detection.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+require('@tensorflow/tfjs-backend-cpu');
+require('@tensorflow/tfjs-backend-webgl');
+import * as cocoSsd from '@tensorflow-models/coco-ssd/dist/index.js';
+
+export default {
+  data() {
+    return {
+      model: null
+    }
+  },
+  async mounted() {
+    this.model = await cocoSsd.load()
+  }
+}
+</script>
