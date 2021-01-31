4555c7f704306c81d433d8198817bf8c1b941431
蒼時弦也 <elct9620@frost.tw>
2021-01-31 14:15:21 +0800

    Set sentry breadcrums logger to ensure not use it

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,7 +2,7 @@
 
 Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
-  # config.breadcrumbs_logger = [:active_support_logger]
+  config.breadcrumbs_logger = []
 
   config.traces_sample_rate = 0.5
 end
